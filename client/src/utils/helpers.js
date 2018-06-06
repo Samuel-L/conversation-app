4891d8524dd8a52e1a72e312d5ee932863614cf6
Samuel <samuell.person@gmail.com>
2018-06-06 22:58:56 +0200

    clientUtils: add apiURL

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -1,3 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 
 export const siteTitle = 'Conversationalist';
+
+export const apiURL = process.env.NODE_ENV === 'production' ? process.env.API_URL : 'http://localhost:8000/api';
+
