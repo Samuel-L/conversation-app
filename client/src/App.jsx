de6de5413632b9f608a9ef976e8b5065eb14814d
Samuel <samuell.person@gmail.com>
2018-06-06 18:06:27 +0200

    add Header component to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
 
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles/';
+import blueGrey from '@material-ui/core/colors/blueGrey';
+import cyan from '@material-ui/core/colors/cyan';
+import red from '@material-ui/core/colors/red';
+
+import Header from './components/Header';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: blueGrey,
+    secondary: cyan,
+    error: red,
+  },
+});
+
 const App = () => (
-  <div>Working!</div>
+  <MuiThemeProvider theme={theme}>
+    <CssBaseline />
+    <Header />
+  </MuiThemeProvider>
 );
 
 export default App;
