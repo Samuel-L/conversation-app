22aedbaaa968b82cf4526300797948702601d680
Samuel <samuell.person@gmail.com>
2018-06-06 17:50:05 +0200

    add eslint to webpack config

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -14,6 +14,7 @@ module.exports = {
   module: {
     rules: [
       { test: [/\.js$/, /\.jsx$/], loader: 'babel-loader', exclude: /node_modules/ },
+      { test: [/\.js?$/, /\.jsx$/], loader: 'eslint-loader', exclude: /node_modules/ },
     ],
   },
   resolve: { extensions: ['.js', '.jsx'] },
