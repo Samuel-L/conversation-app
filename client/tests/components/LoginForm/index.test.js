6f1b9afdf853a38b03a4b196c7559565d5b2faeb
Samuel <samuell.person@gmail.com>
2018-06-07 19:26:18 +0200

    components: add LoginForm and tests

---
+++ client/tests/components/LoginForm/index.test.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { createShallow } from '@material-ui/core/test-utils';
+
+import { LoginForm } from '../../../src/components/LoginForm';
+
+describe('component: LoginForm', () => {
+  const login = jest.fn();
+  const resetState = jest.fn();
+  const classes = { button: '' };
+  let wrapper;
+
+  beforeEach(() => {
+    const shallow = createShallow();
+    wrapper = shallow(
+      <LoginForm
+        login={login}
+        resetState={resetState}
+        classes={classes}
+      />
+    );
+  });
+
+  describe('rendering', () => {
+    it('matches snapshot', () => {
+      const tree = renderer.create(
+        <LoginForm login={login} resetState={resetState} classes={classes} />
+      ).toJSON(); 
+
+      expect(tree).toMatchSnapshot();
+    });
+  });
+
+  describe('functionality', () => {
+    it('takes username input', () => {
+      const usernameInput = wrapper.find('#username').dive().dive().find('#username');
+      usernameInput.simulate('change', { target: { id: 'username', value: 'Username' } });
+
+      expect(wrapper.state('username')).toEqual('Username');
+    });
+
+    it('takes password input', () => {
+      const passwordInput = wrapper.find('#password').dive().dive().find('#password');
+      passwordInput.simulate('change', { target: { id: 'password', value: 'Password' } });
+
+      expect(wrapper.state('password')).toEqual('Password');
+    });
+  });
+});
