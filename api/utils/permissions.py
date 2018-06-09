5d7d204d02ac6cae7e352849e60e3039fd1cc399
Samuel <samuell.person@gmail.com>
2018-06-09 18:35:36 +0200

    api(utils): update IsOwnerOrReadOnly permission

--- api/utils/permissions.py
+++ api/utils/permissions.py
@@ -36,10 +36,12 @@ class IsReceiver(permissions.BasePermission):
 class IsOwnerOrReadOnly(permissions.BasePermission):
     """
     Object-level permission to only allow owners of an object to edit it.
-    Assumes the model instance has an `owner` attribute.
     """
+
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        return obj.owner == request.user
+        return obj.user == request.user
